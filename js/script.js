4e43fa1c76e4fd8298e4ff23bc41225e078e187e
Anton Churauski <example@gmail.com>
2020-06-21 23:31:57 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let incr = 10,
+    decr = 10;
+
+    // ++incr;
+    // --decr;
+
+// console.log(++incr, --decr);
+console.log(5%2);
+
+console.log(2*2*2 == 9);
+
+const pussy = 2*2*2 ==6,
+      juscy = true;
+
+     
+
+
+console.log(!pussy && juscy);
