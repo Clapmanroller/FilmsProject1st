c7f533fe770bfca1e1e251d54f2e503981b807fb
Anton Churauski <example@gmail.com>
2020-06-29 09:46:55 +0300

    после добавления функций

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 'use strict';
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    while (numberOfFilms == " " || numberOfFilms == null || isNaN(numberOfFilms )) {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
 
@@ -9,34 +18,62 @@ const personalMovieDB = {
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    private: false
 };
 
-for (let i = 0; i < 2; i++) {
-
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
+function showMyDB (hidden) {
+    if  (!hidden) {
+        console.log(personalMovieDB); 
     }
+}
 
+showMyDB(personalMovieDB.private);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        const genre = prompt(`Ваш любимый жанр под номером ${i}?`);
+        personalMovieDB.genres[i - 1] = genre;
+    }
 }
-if (personalMovieDB.count < 10) {
-    alert ('Просмотрено довольно мало фильмов');
-    console.log('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    alert ('Вы классический зритель');
-    console.log('Вы классический зритель');
-} else if (personalMovieDB.count > 30) {
-    alert('Вы киноман');
-    console.log('Вы киноман');
+
+ writeYourGenres();
+
+
+function rememberMyFilms () {
+    for (let i = 0; i < 2; i++) {
+
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
     
-} else {
-    alert('Произошла ошибка');
-    console.log('Произошла ошибка');
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    
+    }
 }
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert ('Просмотрено довольно мало фильмов');
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        alert ('Вы классический зритель');
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count > 30) {
+        alert('Вы киноман');
+        console.log('Вы киноман');
+        
+    } else {
+        alert('Произошла ошибка');
+        console.log('Произошла ошибка');
+    }
+}
+
+
+detectPersonalLevel ();
